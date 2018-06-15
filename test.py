ad7d09814d1cf64af49719da7eb844e064e38a5f
zoulingwei <zoulingwei@cyou-inc.com>
2018-06-15 15:16:24 +0800

    fix: some changes

--- test.py
+++ test.py
@@ -87,32 +87,32 @@ def main(_):
     model.load(FLAGS.checkpoint_path)
 
 
-    # ----------test3-------------
-    QAs = get_excel_QAs(FLAGS.input_file)  # 要求excel文件格式，第一个表，第一列id，第二列query,第三列response
-    QA_arrs = converter.QAs_to_arrs(QAs, FLAGS.num_steps)
-    all_samples = converter.samples_for_test3(QA_arrs)
-    indexs = model.test3(all_samples)
-    converter.index_to_QA_and_save(indexs,QAs,FLAGS.checkpoint_path)
-
-    ## ----------test4-------------
-    # libs = get_excel_libs('data/tianlong_libs.xlsx')
-    # libs_arrs = converter.libs_to_arrs(libs,FLAGS.num_steps)
-    # response_matul_state = model.test4_matul(libs_arrs)
+    # # ----------test3-------------
     # QAs = get_excel_QAs(FLAGS.input_file)  # 要求excel文件格式，第一个表，第一列id，第二列query,第三列response
-    # QAY = []
-    # k,n = 0,0
-    # for query,y_response in QAs:
-    #     input_arr,input_len = converter.Q_to_arr(query,FLAGS.num_steps)
-    #     indexs = model.test4(input_arr,input_len, response_matul_state)
-    #     responses = converter.index_to_response(indexs, libs)
-    #     # print('response:',responses)
-    #     QAY.append((query, y_response, responses))
-    #     if responses[0]==y_response:
-    #         k += 1
-    #         print(k)
-    #     n += 1
-    # print('accuracy:',k/float(n))
-    # converter.save_to_excel(QAY, FLAGS.checkpoint_path)
+    # QA_arrs = converter.QAs_to_arrs(QAs, FLAGS.num_steps)
+    # all_samples = converter.samples_for_test3(QA_arrs)
+    # indexs = model.test3(all_samples)
+    # converter.index_to_QA_and_save(indexs,QAs,FLAGS.checkpoint_path)
+
+    # ----------test4-------------
+    libs = get_excel_libs('data/tianlong_libs.xlsx')
+    libs_arrs = converter.libs_to_arrs(libs,FLAGS.num_steps)
+    response_matul_state = model.test4_matul(libs_arrs)
+    QAs = get_excel_QAs(FLAGS.input_file)  # 要求excel文件格式，第一个表，第一列id，第二列query,第三列response
+    QAY = []
+    k,n = 0,0
+    for query,y_response in QAs:
+        input_arr,input_len = converter.Q_to_arr(query,FLAGS.num_steps)
+        indexs = model.test4(input_arr,input_len, response_matul_state)
+        responses = converter.index_to_response(indexs, libs)
+        # print('response:',responses)
+        QAY.append((query, y_response, responses))
+        if responses[0]==y_response:
+            k += 1
+            print(k)
+        n += 1
+    print('accuracy:',k/float(n))
+    converter.save_to_excel(QAY, FLAGS.checkpoint_path)
 
 
     # ## ----------test5------------
